d857cf6cdbf1282403939c84fce175ff5e4a8c5e
Emmanuel Egomson <fegomson@gmail.com>
2025-07-31 18:23:31 -0500

    Implements login and register forms

--- register.php
+++ register.php
@@ -8,6 +8,7 @@
 </head>
 <body>
     <div id="inputContainer">
+        <!-- Login Form -->
         <form action="register.php" id="loginForm" method="POST">
             <h2>Login to your account</h2>
             <div class="form-control">
@@ -20,7 +21,37 @@
             </div>
 
             <button type="submit">Login</button>
-            <p>Don't have an account? <a href="register.php">Register here</a>.</p>
+        </form>
+
+        <!-- Register -->
+        <form action="register.php" id="registerForm" method="POST">
+            <h2>Create a new account</h2>
+            <div class="form-control">
+                <label for="firstName">First Name</label>
+                <input id="firstName" type="text" name="firstName" placeholder="Enter First Name" required>
+            </div>
+            <div class="form-control">
+                <label for="lastName">Last Name</label>
+                <input id="lastName" type="text" name="lastName" placeholder="Enter Last Name" required>
+            </div>
+            <div class="form-control">
+                <label for="email">Email</label>
+                <input id="email" type="email" name="email" placeholder="Enter Email" required>
+            </div>
+            <div class="form-control">
+                <label for="username">Username</label>
+                <input id="username" type="text" name="username" placeholder="Enter Username" required>
+            </div>
+            <div class="form-control">
+                <label for="password">Password</label>
+                <input id="password" type="password" name="password" placeholder="Enter Password" required>
+            </div>
+            <div class="form-control">
+                <label for="confirmPassword">Confirm Password</label>
+                <input id="confirmPassword" type="password" name="confirmPassword" placeholder="Confirm Password" required>
+            </div>
+
+            <button type="submit">Register</button>
         </form>
     </div>
 </body>
