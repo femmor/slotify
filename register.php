cbd847dd6c80078dae768dcd9e2954e9dece163a
Emmanuel Egomson <fegomson@gmail.com>
2025-07-31 22:57:34 -0500

    Refactor registration and login handlers to separate sanitization logic into dedicated files

--- register.php
+++ register.php
@@ -1,62 +1,9 @@
 <?php
 
-// Sanitize username
-// This function removes HTML tags and spaces from the input data
-function sanitizeUsername($data) {
-    $data = strip_tags($data); // Remove HTML tags
-    $data = str_replace(' ', '', $data); // Remove spaces
-    return $data;
-}
-
-// Function to sanitize text input
-// This function removes HTML tags, spaces and capitalizes the first letter from the input data
-function sanitizeTextInput($data) {
-    $data = strip_tags($data); // Remove HTML tags
-    $data = str_replace(' ', '', $data); // Remove spaces
-    $data = ucfirst(strtolower($data)); // Capitalizing first letter
-    return $data;
-}
-
-// Function to sanitize email input
-function sanitizeEmail($data) {
-    $data = filter_var($data, FILTER_SANITIZE_EMAIL); // Sanitize email
-    return $data;
-}
-
-// Function to sanitize password input
-function sanitizePassword($data) {
-    $data = strip_tags($data); // Remove HTML tags
-    return $data;
-}
-
-
-
-
-// Login logic
-if (isset($_POST['loginButton'])) {
-    // Login logic
-    
-}
-
-// Register logic
-if (isset($_POST['registerButton'])) {
-    $username = sanitizeUsername($_POST['username']);
-    $firstName = sanitizeTextInput($_POST['firstName']);
-    $lastName = sanitizeTextInput($_POST['lastName']);
-    $email = sanitizeEmail($_POST['email']);
-    $password = sanitizePassword($_POST['password']);
-    $confirmPassword = sanitizePassword($_POST['confirmPassword']);
-
-    echo "Username: $username<br>";
-    echo "First Name: $firstName<br>";
-    echo "Last Name: $lastName<br>";
-    echo "Email: $email<br>";
-    echo "Password: $password<br>";
-    echo "Confirm Password: $confirmPassword<br>";
-}
-
+// Include the registration handler
+include 'includes/handlers/register-handler.php';
+include 'includes/handlers/login-handler.php';
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
